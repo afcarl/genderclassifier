4af1854132ccb91a0a3fd46ea15618f6e3af7bb6
Divyansh Jha <divyanshj.16@gmail.com>
2017-04-06 11:45:12 +0530

    add accuracy score feature with each prediction

--- genderclassifier.py
+++ genderclassifier.py
@@ -1,4 +1,5 @@
 from sklearn import tree,svm,linear_model
+from sklearn.metrics import accuracy_score
 
 #Height Weight Shoe size
 X = [[181,80,44],[177,70,43],[160,60,38],[154,54,37],[166,65,40],[190,90,47],[175,64,39],[179,70,40],[155,55,37],[171,75,42],[181,45,43]]
@@ -27,20 +28,32 @@ clf4 = clf4.fit(X,Y)
 
 # inp.append(shoeSize)
 
-
+#predict
 prediction = clf.predict([180,60,44])
 prediction2 = clf2.predict([180,60,44])
 prediction3 = clf3.predict([180,60,44])
 prediction4 = clf4.predict([180,60,44])
 
+#To remove this error :-> ValueError: Found input variables with inconsistent numbers of samples: [11, 1]
+xprediction = clf.predict(X)
+xprediction2 = clf2.predict(X)
+xprediction3 = clf3.predict(X)
+xprediction4 = clf4.predict(X)
+
+#calc accuracy
+acc = accuracy_score(Y,xprediction)
+acc2 = accuracy_score(Y,xprediction2)
+acc3 = accuracy_score(Y,xprediction3)
+acc4 = accuracy_score(Y,xprediction4)
+
 print("From DTree")
-print(prediction)
+print(prediction,acc)
 
 print("from svm")
-print(prediction2)
+print(prediction2,acc2)
 
 print('from SGDClassifier')
-print(prediction3)
+print(prediction3,acc3)
 
 print('from perceptron')
-print(prediction4)
+print(prediction4,acc4)
